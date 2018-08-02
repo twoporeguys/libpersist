49e44be86dcfcd287c2b2cb2d8d59a54ccb59bfa
Wojciech Kloska <wojciech.kloska@twoporeguys.com>
2018-08-03 01:50:24 +0200

    ADDED: initial python bindings for py-persist

--- bindings/python/persist.pxd
+++ bindings/python/persist.pxd
@@ -1,16 +1,96 @@
+#
+# Copyright 2018 Wojciech Kloska <shangteus@gmail.com>
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+from libc.stdint cimport *
+
+
+ctypedef bint (*persist_collection_iter_f)(void *arg, const char *name)
+
+
+cdef extern from "rpc/object.h":
+    ctypedef struct rpc_object:
+        pass
+
+    ctypedef rpc_object *rpc_object_t
+
+
 cdef extern from "persist.h":
+    ctypedef struct persist_db:
+        pass
+
+    ctypedef struct persist_collection:
+        pass
+
+    ctypedef struct persist_iter:
+        pass
+
+    ctypedef persist_db *persist_db_t
+    ctypedef persist_collection * persist_collection_t
+    ctypedef persist_iter *persist_iter_t
+
+    void *PERSIST_COLLECTION_ITER(persist_collection_iter_f fn, void *arg)
+
     persist_db_t persist_open(const char *path, const char *driver,
         rpc_object_t params)
     void persist_close(persist_db_t db)
-    persist_collection_t persist_collection_get(persist_db_t db, const char *name)
-    bool persist_collection_exists(persist_db_t db, const char *name)
+    persist_collection_t persist_collection_get(persist_db_t db, const char *name, bint create)
+    bint persist_collection_exists(persist_db_t db, const char *name)
     int persist_collection_remove(persist_db_t db, const char *name)
     rpc_object_t persist_collection_get_metadata(persist_db_t db, const char *name)
     int persist_collection_set_metadata(persist_db_t db, const char *name,
         rpc_object_t metadata)
-    void persist_collections_apply(persist_db_t db)
+    void persist_collections_apply(persist_db_t db, void *applier)
     rpc_object_t persist_get(persist_collection_t col, const char *id)
-    bool persist_query(persist_collection_t col, rpc_object_t query)
+    persist_iter_t persist_query(persist_collection_t col, rpc_object_t query)
     int persist_save(persist_collection_t col, const char *id, rpc_object_t obj)
     int persist_delete(persist_collection_t col, const char *id)
     int persist_get_last_error(char **msgp)
+    void persist_collection_close(persist_collection_t collection)
+    void persist_iter_close(persist_iter_t iter)
+    rpc_object_t persist_iter_next(persist_iter_t iter)
+
+
+cdef class Database(object):
+    cdef persist_db_t db
+
+    @staticmethod
+    cdef bint c_apply_callback(void *arg, const char *name)
+
+
+cdef class Collection(object):
+    cdef persist_collection_t collection
+
+    @staticmethod
+    cdef Collection wrap(persist_collection_t ptr)
+    cdef persist_collection_t unwrap(self) nogil
+
+
+cdef class CollectionIterator(object):
+    cdef persist_iter_t iter
+    cdef object cnt
+
+    @staticmethod
+    cdef CollectionIterator wrap(persist_iter_t iter)
--- bindings/python/persist.pyx
+++ bindings/python/persist.pyx
@@ -1,9 +1,223 @@
+#
+# Copyright 2018 Wojciech Kloska <shangteus@gmail.com>
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+cimport librpc
 cimport persist
 
 
 cdef class Database(object):
-    pass
+    def __init__(self, path, driver, params=None):
+        cdef librpc.Object rpc_params = librpc.Object(params)
+
+        if not isinstance(path, str):
+            raise TypeError('Path needs to be a string')
+
+        if not isinstance(driver, str):
+            raise TypeError('Driver needs to be a string')
+
+        if not isinstance(params, (librpc.Object, None)):
+            raise TypeError('Params needs to be a librpc Object or None')
+
+        self.db = persist_open(path.encode('utf-8'), driver.encode('utf-8'), rpc_params.unwrap())
+
+        if self.db == <persist_db_t>NULL:
+            raise ValueError('Cannot open database {}'.format(path))
+
+    def __dealloc__(self):
+        if self.db != <persist_db_t>NULL:
+            persist_close(self.db)
+
+    def collection_exists(self, name):
+        if not isinstance(name, str):
+            raise TypeError('Collection name needs to be a string')
+
+        return persist_collection_exists(self.db, name.encode('utf-8'))
+
+    def get_collection(self, name, create=False):
+        cdef persist_collection_t collection
+
+        if not isinstance(name, str):
+            raise TypeError('Collection name needs to be a string')
+
+        if not create and not persist_collection_exists(self.db, name.encode('utf-8')):
+            raise NameError('Collection {} does not exist'.format(name))
+
+        collection = persist_collection_get(self.db, name.encode('utf-8'), create)
+
+        return Collection.wrap(collection)
+
+    def create_collection(self, name):
+        if not isinstance(name, str):
+            raise TypeError('Collection name needs to be a string')
+
+        if persist_collection_exists(self.db, name.encode('utf-8')):
+            raise ValueError('Collection {} already exists'.format(name))
+
+        return self.get_collection(name, True)
+
+    def remove_collection(self, name):
+        if not isinstance(name, str):
+            raise TypeError('Collection name needs to be a string')
+
+        if not persist_collection_exists(self.db, name.encode('utf-8')):
+            raise NameError('Collection {} does not exist'.format(name))
+
+        persist_collection_remove(self.db, name.encode('utf-8'))
+
+    def get_collection_metadata(self, name):
+        cdef rpc_object_t metadata
+
+        if not isinstance(name, str):
+            raise TypeError('Collection name needs to be a string')
+
+        if not persist_collection_exists(self.db, name.encode('utf-8')):
+            raise NameError('Collection {} does not exist'.format(name))
+
+        metadata = persist_collection_get_metadata(self.db, name.encode('utf-8'))
+
+        return librpc.Object.wrap(metadata).unpack()
+
+    def set_collection_metadata(self, name, metadata):
+        cdef librpc.Object rpc_metadata = librpc.Object(metadata)
+
+        if not isinstance(name, str):
+            raise TypeError('Collection name needs to be a string')
+
+        if not persist_collection_exists(self.db, name.encode('utf-8')):
+            raise NameError('Collection {} does not exist'.format(name))
+
+        persist_collection_set_metadata(self.db, name.encode('utf-8'), rpc_metadata.unwrap())
+
+    def list_collections(self):
+        names = []
+        def collect(name):
+            names.append(name)
+
+        persist_collections_apply(
+            self.db,
+            PERSIST_COLLECTION_ITER(
+                <persist_collection_iter_f>Database.c_apply_callback,
+                <void *>collect
+            )
+        )
+
+        return names
+
+    @staticmethod
+    cdef bint c_apply_callback(void *arg, const char *name):
+        cdef object cb = <object>arg
+        cb(name)
 
 
 cdef class Collection(object):
-    pass
+    def __dealloc__(self):
+        if self.collection != <persist_collection_t>NULL:
+            persist_collection_close(self.collection)
+
+    @staticmethod
+    cdef Collection wrap(persist_collection_t ptr):
+        cdef Collection ret
+
+        if ptr == <persist_collection_t>NULL:
+            return None
+
+        ret = Collection.__new__(Collection)
+        ret.collection = ptr
+
+        return ret
+
+    cdef persist_collection_t unwrap(self) nogil:
+        return self.collection
+
+    def get(self, id, default=None):
+        cdef rpc_object_t ret
+
+        if not isinstance(id, str):
+            raise TypeError('Id needs to be a string')
+
+        ret = persist_get(self.collection, id.encode('utf-8'))
+
+        if ret == <rpc_object_t>NULL:
+            return default
+
+        return librpc.Object.wrap(ret).unpack()
+
+    def set(self, id, value):
+        cdef librpc.Object rpc_value
+
+        if not isinstance(id, str):
+            raise TypeError('Id needs to be a string')
+
+        rpc_value = librpc.Object(value)
+        persist_save(self.collection, id.encode('utf-8'), rpc_value.unwrap())
+
+    def delete(self, id):
+        if not isinstance(id, str):
+            raise TypeError('Id needs to be a string')
+
+        persist_delete(self.collection, id.encode('utf-8'))
+
+    def query(self, params):
+        cdef persist_iter_t iter
+        cdef librpc.Object rpc_params = librpc.Object(params)
+
+        iter = persist_query(self.collection, rpc_params.unwrap())
+        return CollectionIterator.wrap(iter)
+
+
+cdef class CollectionIterator(object):
+    def __dealloc__(self):
+        if self.iter != <persist_iter_t>NULL:
+            persist_iter_close(self.iter)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        cdef rpc_object_t result
+
+        if not self.cnt:
+            raise StopIteration
+
+        result = persist_iter_next(self.iter)
+        if result == <rpc_object_t>NULL:
+            self.cnt = False
+            raise StopIteration
+
+        return librpc.Object.wrap(result).unpack()
+
+    def next(self):
+        return self.__next__()
+
+    @staticmethod
+    cdef CollectionIterator wrap(persist_iter_t iter):
+        cdef CollectionIterator ret
+
+        ret = CollectionIterator.__new__(CollectionIterator)
+        ret.iter = iter
+        ret.cnt = True
+
+        return ret
